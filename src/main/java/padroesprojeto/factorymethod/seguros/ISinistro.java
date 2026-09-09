54ebc54846e96bc9d8593051a234214d51637045
HUGO NOGUEIRA CARVALHO <90403398+hugocarv07@users.noreply.github.com>
2026-09-09 18:04:33 -0300

    Update ISinistro.java

--- src/main/java/padroesprojeto/factorymethod/seguros/ISinistro.java
+++ src/main/java/padroesprojeto/factorymethod/seguros/ISinistro.java
@@ -1,10 +1,5 @@
 package padroesprojeto.factorymethod.seguros;
 
-/**
- * Produto abstrato do padrão Factory Method.
- * Toda apólice de seguro que pode gerar um sinistro processável
- * deve implementar este contrato.
- */
 public interface ISinistro {
 
     String processar();
